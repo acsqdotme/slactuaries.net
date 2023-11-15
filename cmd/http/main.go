2c35a9734ddf2f1ae0d8c2b819f1a21c582dbd45
Ángel Castañeda <angel@acsq.me>
2023-11-15 16:40:53 -0600

    Format main.go

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -7,15 +7,17 @@ import (
 )
 
 func main() {
-  addr := flag.String("addr", ":4004", "Server Port Address")
-  flag.Parse()
+	addr := flag.String("addr", ":4004", "Server Port Address")
+	flag.Parse()
 
-  mux := http.NewServeMux()
-  mux.HandleFunc("/", pageHandler)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", pageHandler)
+	// todo make file server handler func
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-  log.Printf("Starting server on %s", *addr)
-  err := http.ListenAndServe(*addr, mux)
-  log.Fatal(err)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
 }
