4213601eab1a31c58e5a9bf19198a94385dcd683
Ángel Castañeda <angel@acsq.me>
2023-11-15 16:43:13 -0600

    Add template helper funcs from other sites
    normal template stuff is too painful without serveTMPL and bindTMPL.

--- cmd/http/handlers.go
+++ cmd/http/handlers.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"bytes"
+	"errors"
+	"html/template"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-func doesFileExist(filePath string) bool {
-  info, err := os.Stat(filePath)
-  if err != nil || info.IsDir() {
-    return false
-  }
-  return true
+func doesFileExist(pathToFile string) bool {
+	info, err := os.Stat(filepath.Clean(pathToFile))
+	if err != nil || info.IsDir() {
+		return false
+	}
+	return true
+}
+
+func bindTMPL(files ...string) (*template.Template, error) {
+	for _, checkFile := range files {
+		if !doesFileExist(checkFile) {
+			return nil, errors.New("Template file missing " + checkFile)
+		}
+	}
+
+	tmpl, err := template.New("notSureWhatThisDoes").Funcs(nil).ParseFiles(files...)
+	if err != nil {
+		return nil, err
+	}
+
+	return tmpl, nil
+}
+
+func serveTMPL(w http.ResponseWriter, r *http.Request, tmpl *template.Template, data map[string]interface{}) {
+	var buf bytes.Buffer
+	err := tmpl.ExecuteTemplate(&buf, "base", data)
+	if err != nil {
+		log.Println(err.Error())
+		// fancyErrorHandler(w, r, http.StatusInternalServerError)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError) // TODO add proper error pages
+		return
+	}
+	buf.WriteTo(w)
 }
 
 func pageHandler(w http.ResponseWriter, r *http.Request) {
