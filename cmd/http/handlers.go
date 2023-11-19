f38e707e4fdc39a4a2579ffdaf4f73ca9e8d19a0
Ángel Castañeda <angel@acsq.me>
2023-11-19 16:14:10 -0600

    Add topic handler func
    this is a two for one func for both the index of topics as well as any
    lessons beneath.

--- cmd/http/handlers.go
+++ cmd/http/handlers.go
@@ -3,12 +3,15 @@ package main
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"slactuaries.net/pkgs/direr"
 )
 
 const (
@@ -94,7 +97,65 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 	serveTMPL(w, r, tmpl, nil)
 }
 
-func examHandler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type","text/html; charset=utf-8")
+func topicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
+	path := strings.Split(r.URL.Path, "/")
+	topic := path[1]
+
+	if !doesDirExist(filepath.Join(htmlDir, "topics", topic)) {
+		http.Error(w, "page not found", http.StatusNotFound)
+		return
+	}
+
+	if len(path) == 3 && path[2] == "" {
+		http.Redirect(w, r, "/"+topic, 302)
+		return
+	}
+
+	data := make(map[string]any)
+	t, err := direr.MakeTree(filepath.Join(htmlDir, "topics", topic, "lessons"), ".md")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	data["Tree"] = *t
+
+	if len(path) < 3 {
+		tmpl, err := bindTMPL(
+			filepath.Join(htmlDir, "base"+tmplFileExt),
+			filepath.Join(htmlDir, "topics", topic, "index"+tmplFileExt),
+			filepath.Join(htmlDir, "partials", "ulTree"+tmplFileExt),
+		)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		serveTMPL(w, r, tmpl, data)
+		return
+	}
+
+	page := strings.TrimPrefix(r.URL.Path, "/"+topic+"/") // TODO make this less ugly but still removing leading slash
+
+	if !doesFileExist(filepath.Join(htmlDir, "topics", topic, "lessons", page+tmplFileExt)) { // TODO change how this checks for files
+		fmt.Println("can't find", filepath.Join(htmlDir, "topics", topic, "lessons", page+tmplFileExt))
+		http.Error(w, "page not found in dir", http.StatusNotFound)
+		return
+	}
+
+	tmpl, err := bindTMPL(
+		filepath.Join(htmlDir, "base"+tmplFileExt),
+		filepath.Join(htmlDir, "topics", topic, "lessons", page+tmplFileExt),
+	)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	serveTMPL(w, r, tmpl, data)
+  return
 }
